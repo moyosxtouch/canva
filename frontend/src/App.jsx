7deda6a83c7063b20f0737f77667600af0e5ebff
moyo <moyosxtouch@gmail.com>
2025-02-25 20:01:19 -0600

    change in sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,28 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Index from "./pages/index";
 
+import Layout from "./pages/Layout";
+import Home from "./components/Home";
+import Templates from "./components/Templates";
+import Projects from "./components/Projects";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Index />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/templates",
+        element: <Templates />,
+      },
+      {
+        path: "/projects",
+        element: <Projects />,
+      },
+    ],
   },
 ]);
 function App() {
---
+++ frontend/src/components/Templates.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Templates = () => {
+  return (
+    <div>
+      <h1>Templates</h1>
+      <p>This is a template component.</p>
+    </div>
+  );
+};
+
+export default Templates;
